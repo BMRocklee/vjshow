85b3f328822552b8525fe5dffc73a4451504994f
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-23 17:00:49 +0700

    Update UserRepository.java

--- marketplace/src/main/java/com/vjshow/marketplace/repository/UserRepository.java
+++ marketplace/src/main/java/com/vjshow/marketplace/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.vjshow.marketplace.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vjshow.marketplace.entity.UserEntity;
 import com.vjshow.marketplace.enums.AuthProviderEnum;
 import com.vjshow.marketplace.enums.Role;
+import com.vjshow.marketplace.enums.UserStatusEnum;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
@@ -19,4 +21,12 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	Optional<UserEntity> findByProviderAndProviderId(AuthProviderEnum provider, String providerId);
 	
 	long countByRole(Role role);
+
+	List<UserEntity> findByStatusAndNameContainingIgnoreCase(UserStatusEnum status, String keyword);
+
+	List<UserEntity> findByNameContainingIgnoreCase(String keyword);
+	
+	List<UserEntity> findByStatus(UserStatusEnum status);
+
+	long countByStatus(UserStatusEnum status);
 }
