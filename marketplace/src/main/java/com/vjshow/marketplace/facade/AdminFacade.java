07315c27915e1f33f2706519bf2ee4386727f827
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-23 16:59:47 +0700

    Expand AdminFacade with additional methods

--- marketplace/src/main/java/com/vjshow/marketplace/facade/AdminFacade.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/AdminFacade.java
@@ -8,3 +8,29 @@ public interface AdminFacade {
 
   public CreatorDashboardResponse getCreatorDashboard(); 
 }
+package com.vjshow.marketplace.facade;
+
+import com.vjshow.marketplace.dto.response.AdminDashboardResponseDto;
+import com.vjshow.marketplace.dto.response.CreatorPaymentsResponse;
+import com.vjshow.marketplace.dto.response.CreatorRegisterInfoResponse;
+import com.vjshow.marketplace.dto.response.UserAdminResponse;
+import com.vjshow.marketplace.enums.CreatorStatus;
+import com.vjshow.marketplace.enums.UserStatusEnum;
+
+public interface AdminFacade {
+	public AdminDashboardResponseDto getAdminDashboardStatInfo();
+
+	public CreatorRegisterInfoResponse getCreatorDashboard(CreatorStatus status, String keyword);
+
+	public CreatorPaymentsResponse getCreatorPayments();
+
+	public void approveCreator(Long creatorId);
+
+	public void rejectCreator(Long creatorId);
+
+	public UserAdminResponse getUserList(String keyword, UserStatusEnum status);
+
+	public void blockUser(Long id);
+
+	public void activeUser(Long id);
+}
