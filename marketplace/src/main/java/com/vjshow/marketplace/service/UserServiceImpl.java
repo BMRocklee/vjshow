d5a73610dbe2de47803bb395f7dd5e0485bdd44f
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-23 17:02:21 +0700

    Add user filtering by keyword and status in UserServiceImpl
    Enhance getUsers method to filter users by keyword and status.

--- marketplace/src/main/java/com/vjshow/marketplace/service/UserServiceImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.vjshow.marketplace.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.vjshow.marketplace.dto.response.UserAdminDTO;
+import com.vjshow.marketplace.dto.response.UserAdminResponse;
+import com.vjshow.marketplace.dto.response.UserStatsDTO;
 import com.vjshow.marketplace.entity.UserEntity;
 import com.vjshow.marketplace.enums.AuthProviderEnum;
 import com.vjshow.marketplace.enums.Role;
+import com.vjshow.marketplace.enums.UserStatusEnum;
 import com.vjshow.marketplace.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -33,4 +39,42 @@ public class UserServiceImpl implements UserService {
 		return userRepository.count();
 	}
 
+	@Override
+	public UserAdminResponse getUsers(String keyword, UserStatusEnum status) {
+		List<UserEntity> users;
+
+	    boolean hasKeyword = keyword != null && !keyword.isBlank();
+
+	    if (status != null && hasKeyword) {
+	        users = userRepository.findByStatusAndNameContainingIgnoreCase(status, keyword);
+	    } else if (status != null) {
+	        users = userRepository.findByStatus(status);
+	    } else if (hasKeyword) {
+	        users = userRepository.findByNameContainingIgnoreCase(keyword);
+	    } else {
+	        users = userRepository.findAll();
+	    }
+
+	    List<UserAdminDTO> list = users.stream().map(user ->
+	        UserAdminDTO.builder()
+	            .id(user.getId())
+	            .name(user.getName())
+	            .email(user.getEmail())
+	            .avatar(user.getPicture())
+	            .uid(user.getPublicId().toString())
+	            .role(user.getRole().name())
+	            .createdAt(user.getCreatedAt())
+	            .status(user.getStatus())
+	            .build()
+	    ).toList();
+
+	    // stats
+	    UserStatsDTO stats = UserStatsDTO.builder()
+	        .totalUsers(userRepository.count())
+	        .active(userRepository.countByStatus(UserStatusEnum.ACTIVE))
+	        .blocked(userRepository.countByStatus(UserStatusEnum.BLOCKED))
+	        .build();
+
+	    return new UserAdminResponse(stats, list);
+	}
 }
