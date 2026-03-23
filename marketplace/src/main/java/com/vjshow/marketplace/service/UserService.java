0d99423eb5dd35de9c2204a2f00ac71460f484cb
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-23 17:02:05 +0700

    Add getUsers method to UserService interface

--- marketplace/src/main/java/com/vjshow/marketplace/service/UserService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/UserService.java
@@ -1,10 +1,14 @@
 package com.vjshow.marketplace.service;
 
+import com.vjshow.marketplace.dto.response.UserAdminResponse;
 import com.vjshow.marketplace.entity.UserEntity;
 import com.vjshow.marketplace.enums.AuthProviderEnum;
+import com.vjshow.marketplace.enums.UserStatusEnum;
 
 public interface UserService {
 	public UserEntity findOrCreateOAuthUser(AuthProviderEnum provider, String providerId, String email, String name, String picture);
 	
 	public long getTotalUsers();
+	
+	public UserAdminResponse  getUsers(String keyword, UserStatusEnum status);
 }
