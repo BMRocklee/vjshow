c6e4da766611036cb5fbc87c5431789f996c24f0
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-24 16:45:24 +0700

    Add files via upload

---
+++ marketplace/src/main/java/com/vjshow/marketplace/entity/PaymentEntity.java
@@ -0,0 +1,40 @@
+package com.vjshow.marketplace.entity;
+
+import java.time.LocalDateTime;
+
+import com.vjshow.marketplace.enums.PaymentStatusEnum;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "payments")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentEntity {
+	
+	@Id
+	private String id; // UUID
+
+	private Long amount;
+
+	private String content; // nội dung chuyển khoản (VJSHOW-xxx)
+
+    @Enumerated(EnumType.STRING)
+	private PaymentStatusEnum status; // PENDING | PAID | EXPIRED
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime paidAt;
+}
