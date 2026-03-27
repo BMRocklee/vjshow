2f31f67d5b63ecfd078736fbed1f5b5be96e3d4c
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-27 16:36:53 +0700

    Refactor ProductService to interface and add method

--- marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
@@ -1,19 +1,12 @@
 package com.vjshow.marketplace.service;
 
-import org.springframework.stereotype.Service;
-
+import com.vjshow.marketplace.dto.request.CompleteUploadRequest;
+import com.vjshow.marketplace.entity.CreatorEntity;
 import com.vjshow.marketplace.entity.ProductEntity;
-import com.vjshow.marketplace.exception.LogicException;
-import com.vjshow.marketplace.repository.ProductRepository;
 
-import lombok.RequiredArgsConstructor;
+public interface ProductService {
 
-@Service
-@RequiredArgsConstructor
-public class ProductService {
-	private final ProductRepository productRepo;
-
-	public ProductEntity getById(Long id) {
-		return productRepo.findById(id).orElseThrow(() -> new LogicException("NOT_FOUND", "không tồn tại sản phẩm"));
-	}
+	public ProductEntity getById(Long id);
+	
+	ProductEntity createProduct(CompleteUploadRequest request, CreatorEntity creator);
 }
