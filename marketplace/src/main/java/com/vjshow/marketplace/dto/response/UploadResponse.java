25f06562304a99feb7b2fc7e5f60c776046e6506
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-27 16:40:44 +0700

    Add files via upload

---
+++ marketplace/src/main/java/com/vjshow/marketplace/dto/response/UploadResponse.java
@@ -0,0 +1,18 @@
+package com.vjshow.marketplace.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UploadResponse {
+
+	private String key;
+
+	private String presignedUrl;
+
+}
