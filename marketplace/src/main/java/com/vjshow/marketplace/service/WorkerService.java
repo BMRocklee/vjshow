d442e9bd2fbfdc3ac041cbcfb8438a7efaf68492
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-27 16:37:10 +0700

    Refactor WorkerService to use WebClient

--- marketplace/src/main/java/com/vjshow/marketplace/service/WorkerService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/WorkerService.java
@@ -2,30 +2,21 @@ package com.vjshow.marketplace.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class WorkerService {
-	 private final RestTemplate restTemplate = new RestTemplate();
 
-	    public void sendVideoJob(String url) {
-	        Map<String, String> body = Map.of("url", url);
+	private final WebClient webClient;
 
-	        restTemplate.postForObject(
-	            "http://localhost:3001/queue/video",
-	            body,
-	            String.class
-	        );
-	    }
+	public WorkerService(@Value("${worker.url}") String workerUrl) {
+		this.webClient = WebClient.builder().baseUrl(workerUrl).build();
+	}
 
-	    public void sendImageJob(String url) {
-	        Map<String, String> body = Map.of("url", url);
-
-	        restTemplate.postForObject(
-	            "http://localhost:3001/queue/image",
-	            body,
-	            String.class
-	        );
-	    }
+	public void sendJob(String key, String type) {
+		webClient.post().uri("/queue/job").bodyValue(Map.of("key", key, "type", type)).retrieve()
+				.bodyToMono(String.class).subscribe();
+	}
 }
