77ea1b75070bafcfe9de61bd33512d8ab7b12772
duytrinh227 <duytrinh227@gmail.com>
2026-03-30 04:45:13 +0700

    update

--- marketplace/src/main/java/com/vjshow/marketplace/controller/TestController.java
+++ marketplace/src/main/java/com/vjshow/marketplace/controller/TestController.java
@@ -1,16 +1,10 @@
 package com.vjshow.marketplace.controller;
 
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.vjshow.marketplace.service.CloudFlareService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,13 +13,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TestController {
 	
-	private final CloudFlareService r2Service;
-	
-	@PostMapping("/upload-test")
-	public String upload(@RequestParam MultipartFile file) {
-	    String key = "test/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
-	    return r2Service.upload(file, key);
-	}
+//	private final CloudFlareService r2Service;
+//	
+//	@PostMapping("/upload-test")
+//	public String upload(@RequestParam MultipartFile file) {
+//	    String key = "test/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+//	    return r2Service.upload(file, key);
+//	}
 	
 	@GetMapping("/data-deletion")
     public Map<String, String> delete() {
