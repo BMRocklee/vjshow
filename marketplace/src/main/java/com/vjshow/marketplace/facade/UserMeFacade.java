ca4e92a3f99aaf6e0ce3bb44af20dda624af2333
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-30 16:08:41 +0700

    Add methods to UserMeFacade for product management

--- marketplace/src/main/java/com/vjshow/marketplace/facade/UserMeFacade.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/UserMeFacade.java
@@ -1,10 +1,20 @@
 package com.vjshow.marketplace.facade;
 
+import java.util.List;
+
 import org.springframework.security.core.Authentication;
 
+import com.vjshow.marketplace.dto.request.ProductRequest;
 import com.vjshow.marketplace.dto.response.UserMeResponseDto;
+import com.vjshow.marketplace.dto.response.UserProductResponse;
+import com.vjshow.marketplace.entity.ProductEntity;
 
 public interface UserMeFacade {
 	public UserMeResponseDto getUserMe(Authentication authentication);
+	
+	public List<UserProductResponse> getMyProducts(Authentication authentication);
 
+	public ProductEntity updateMyProduct(Long id, ProductRequest request);
+
+	public void deleteMyProduct(Long id);
 }
