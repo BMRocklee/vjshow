3311299bb93b3c8122c48b744be56a37b69f7df5
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-30 16:08:19 +0700

    Add product management endpoints to UserMeController

--- marketplace/src/main/java/com/vjshow/marketplace/controller/UserMeController.java
+++ marketplace/src/main/java/com/vjshow/marketplace/controller/UserMeController.java
@@ -1,14 +1,22 @@
 package com.vjshow.marketplace.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vjshow.marketplace.dto.request.ProductRequest;
 import com.vjshow.marketplace.dto.response.UserMeResponseDto;
+import com.vjshow.marketplace.dto.response.UserProductResponse;
 import com.vjshow.marketplace.facade.UserMeFacade;
 
 import lombok.RequiredArgsConstructor;
@@ -32,4 +40,26 @@ public class UserMeController {
 		return ResponseEntity.ok(userMe);
 
 	}
+	
+	@GetMapping("/me/products")
+	public ResponseEntity<?> getMyProduct(Authentication authentication) {
+
+		List<UserProductResponse> listProducts = userMeFacade.getMyProducts(authentication);
+
+		return ResponseEntity.ok(listProducts);
+
+	}
+	
+	@PutMapping("/me/products/{id}")
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ProductRequest request) {
+		return ResponseEntity.ok(userMeFacade.updateMyProduct(id, request));
+	}
+	
+	@DeleteMapping("/me/products/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+		userMeFacade.deleteMyProduct(id);
+        return ResponseEntity.ok().build();
+    }
+	
+	
 }
