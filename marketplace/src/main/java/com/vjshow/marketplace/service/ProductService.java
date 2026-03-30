d5945a4b8ed2860498cb9f2bf226ae7d24bcfdff
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-30 16:09:40 +0700

    Add methods to ProductService interface

--- marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.vjshow.marketplace.service;
 
+import java.util.List;
+
 import com.vjshow.marketplace.dto.request.CompleteUploadRequest;
+import com.vjshow.marketplace.dto.request.ProductRequest;
 import com.vjshow.marketplace.entity.CreatorEntity;
 import com.vjshow.marketplace.entity.ProductEntity;
 
@@ -8,5 +11,11 @@ public interface ProductService {
 
 	public ProductEntity getById(Long id);
 	
-	ProductEntity createProduct(CompleteUploadRequest request, CreatorEntity creator);
+	public ProductEntity createProduct(CompleteUploadRequest request, CreatorEntity creator);
+	
+	public List<ProductEntity> getByCreatorId(Long creatorId);
+    
+	public ProductEntity update(Long id, ProductRequest request);
+    
+	public void delete(Long id);
 }
