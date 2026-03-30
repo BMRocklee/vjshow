6f43acc7ff4cf24322e142c90b38f987be585b9b
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-30 16:09:00 +0700

    Refactor UserMeFacadeImpl for authentication handling

--- marketplace/src/main/java/com/vjshow/marketplace/facade/UserMeFacadeImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/UserMeFacadeImpl.java
@@ -1,18 +1,35 @@
 package com.vjshow.marketplace.facade;
 
+import java.util.List;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import com.vjshow.marketplace.dto.request.ProductRequest;
 import com.vjshow.marketplace.dto.response.UserMeResponseDto;
+import com.vjshow.marketplace.dto.response.UserProductResponse;
+import com.vjshow.marketplace.entity.CreatorEntity;
+import com.vjshow.marketplace.entity.ProductEntity;
 import com.vjshow.marketplace.entity.UserEntity;
+import com.vjshow.marketplace.exception.LogicException;
+import com.vjshow.marketplace.mapper.ProductMapper;
+import com.vjshow.marketplace.service.CreatorService;
+import com.vjshow.marketplace.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class UserMeFacadeImpl implements UserMeFacade {
+	
+	private final ProductService productService;
+    private final CreatorService creatorService;
+    private final ProductMapper productMapper;
 
 	@Override
 	public UserMeResponseDto getUserMe(Authentication authentication) {
 		if (authentication == null || !authentication.isAuthenticated()) {
-			return null;
+			 throw new LogicException("Unauthenticated", "bạn chưa đăng nhập hoặc session đã hết hạn");
 		}
 
 		UserEntity userme = (UserEntity) authentication.getPrincipal();
@@ -21,6 +38,31 @@ public class UserMeFacadeImpl implements UserMeFacade {
 				.email(userme.getEmail()).picture(userme.getPicture()).role(userme.getRole().name()).build();
 
 		return userMeResponseDto;
+	}
+
+	@Override
+	public List<UserProductResponse> getMyProducts(Authentication authentication) {
+		if (authentication == null || !authentication.isAuthenticated()) {
+			 throw new LogicException("Unauthenticated", "bạn chưa đăng nhập hoặc session đã hết hạn");
+		}
+
+		UserEntity userme = (UserEntity) authentication.getPrincipal();
+		CreatorEntity creator =  creatorService.getByUserId(userme.getId());
+        var products = productService.getByCreatorId(creator.getId());
+		
+        return products.stream()
+                .map(productMapper::toResponse)
+                .toList();
+	}
+
+	@Override
+	public ProductEntity updateMyProduct(Long id, ProductRequest request) {
+		return productService.update(id, request);
+	}
+
+	@Override
+	public void deleteMyProduct(Long id) {
+		productService.delete(id);
 	}
 
 }
