1e08e55a06613b008b30fae37c5f0a56d611b95a
duytrinh227 <duytrinh227@gmail.com>
2026-04-01 01:03:47 +0700

    [trinh.dd] add file

---
+++ marketplace/src/main/java/com/vjshow/marketplace/service/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.vjshow.marketplace.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.vjshow.marketplace.entity.PaymentEntity;
+import com.vjshow.marketplace.repository.PaymentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+	private final PaymentRepository paymentRepo;
+
+	@Override
+	public void markSuccess(PaymentEntity payment) {
+		paymentRepo.save(payment);
+	}
+
+	@Override
+	public Optional<PaymentEntity> findByContent(String content) {
+		// TODO Auto-generated method stub
+		return paymentRepo.findbyContent(content);
+	}
+
+}
