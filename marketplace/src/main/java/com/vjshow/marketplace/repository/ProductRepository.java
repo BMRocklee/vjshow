85fdf9bc99474b6cf88f9b6eb47211207f1ddb90
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-31 16:35:13 +0700

    Add methods to filter products by status

--- marketplace/src/main/java/com/vjshow/marketplace/repository/ProductRepository.java
+++ marketplace/src/main/java/com/vjshow/marketplace/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vjshow.marketplace.entity.ProductEntity;
+import com.vjshow.marketplace.enums.ProductStatusEnum;
 import com.vjshow.marketplace.enums.ProductTypeEnum;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
@@ -14,7 +15,12 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 	List<ProductEntity> findByNameContainingIgnoreCase(String keyword);
 
 	List<ProductEntity> findByTypeAndNameContainingIgnoreCase(ProductTypeEnum valueOf, String keyword);
-	
+
+	List<ProductEntity> findByStatusAndNameContainingIgnoreCase(ProductStatusEnum status, String keyword);
+
+	List<ProductEntity> findByStatusAndTypeAndNameContainingIgnoreCase(ProductStatusEnum status, ProductTypeEnum type,
+			String keyword);
+
 	List<ProductEntity> findByCreatorIdOrderByCreatedAtDesc(Long creatorId);
 
 }
