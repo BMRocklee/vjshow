bdc9d5eb35ca06206f5259bc54b2a8889b7db0cb
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-03-31 16:34:44 +0700

    Refactor ProductController to use ProductFacade

--- marketplace/src/main/java/com/vjshow/marketplace/controller/ProductController.java
+++ marketplace/src/main/java/com/vjshow/marketplace/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.vjshow.marketplace.controller;
 
-import java.util.List;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vjshow.marketplace.dto.request.HandleFileDoneRequest;
-import com.vjshow.marketplace.entity.ProductEntity;
-import com.vjshow.marketplace.enums.ProductStatusEnum;
-import com.vjshow.marketplace.enums.ProductTypeEnum;
-import com.vjshow.marketplace.exception.LogicException;
-import com.vjshow.marketplace.repository.ProductRepository;
+import com.vjshow.marketplace.facade.ProductFacade;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,37 +19,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductController {
 
-	private final ProductRepository productRepository;
-	
-	@GetMapping
-	public List<ProductEntity> getProducts(@RequestParam(required = false) String type,
-			@RequestParam(required = false, defaultValue = "") String keyword) {
-		if (type == null || type.isEmpty()) {
-			return productRepository.findByNameContainingIgnoreCase(keyword);
-		}
+	private final ProductFacade productFacade;
 
-		return productRepository.findByTypeAndNameContainingIgnoreCase(ProductTypeEnum.valueOf(type), keyword);
+	@GetMapping
+	public ResponseEntity<?> getProducts(@RequestParam(required = false) String type,
+			@RequestParam(required = false, defaultValue = "") String keyword) {
+		return ResponseEntity.ok(productFacade.getProducts(type, keyword));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ProductEntity getById(@PathVariable Long id) {
-	    return productRepository.findById(id)
-	        .orElseThrow(() -> new LogicException("NOT_FOUND","Không tìm thấy sản phẩm"));
+	public ResponseEntity<?> getById(@PathVariable Long id) {
+		return ResponseEntity.ok(productFacade.getById(id));
 	}
 
 	@PostMapping("/done")
 	public void done(@RequestBody HandleFileDoneRequest fileInfo) {
-		String fileKey = fileInfo.getFileKey();
-
-		ProductEntity product = productRepository.findByFileKey(fileKey);
-
-		product.setPreviewUrl(fileInfo.getPreviewUrl());
-		product.setThumbnailUrl(fileInfo.getThumbUrl());
-		product.setWidth(fileInfo.getWidth());
-		product.setHeight(fileInfo.getHeight());
-		product.setDuration(fileInfo.getDuration());
-		
-		product.setStatus(ProductStatusEnum.DONE);
-		productRepository.save(product);
+		productFacade.markDone(fileInfo);
 	}
 }
