a5f8e82fae5812fc9c8824483c17a09757afeda5
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-01 16:27:57 +0700

    Add payment status check and expiration handling
    Implement payment status checking with expiration logic and download URL generation.

--- marketplace/src/main/java/com/vjshow/marketplace/service/PaymentServiceImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/PaymentServiceImpl.java
@@ -1,10 +1,17 @@
 package com.vjshow.marketplace.service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
+import com.vjshow.marketplace.dto.response.PaymentStatusResponse;
+import com.vjshow.marketplace.entity.OrderEntity;
 import com.vjshow.marketplace.entity.PaymentEntity;
+import com.vjshow.marketplace.entity.ProductEntity;
+import com.vjshow.marketplace.enums.PaymentStatusEnum;
+import com.vjshow.marketplace.exception.LogicException;
+import com.vjshow.marketplace.repository.OrderRepository;
 import com.vjshow.marketplace.repository.PaymentRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +21,10 @@ import lombok.RequiredArgsConstructor;
 public class PaymentServiceImpl implements PaymentService {
 
 	private final PaymentRepository paymentRepo;
+	
+	private final OrderRepository orderRepo;
+	
+	private final CloudFlareService storageService;
 
 	@Override
 	public void markSuccess(PaymentEntity payment) {
@@ -24,6 +35,62 @@ public class PaymentServiceImpl implements PaymentService {
 	public Optional<PaymentEntity> findByContent(String content) {
 		// TODO Auto-generated method stub
 		return paymentRepo.findByContent(content);
+	}
+
+	@Override
+	public PaymentStatusResponse checkPayment(String paymentId, Long userId) {
+		
+		// 1. find payment
+        PaymentEntity payment = paymentRepo.findById(paymentId)
+                .orElseThrow(() -> new LogicException("NOT_FOUND","không tìm thấy paymentID"));
+
+       // 2. find order từ payment
+        OrderEntity order = orderRepo.findByPaymentId(paymentId)
+                .orElseThrow(() -> new LogicException("NOT_FOUND","không tìm thấy Order"));
+        
+        // 3. SECURITY - đúng user
+        if (!order.getBuyer().getId().equals(userId)) {
+            throw new LogicException("NOT_FOUND","bạn chưa có hóa đơn mua hàng");
+        }
+        
+        // 4. EXPIRE PAYMENT (5 phút)
+        if (payment.getStatus() == PaymentStatusEnum.PENDING) {
+
+            if (payment.getCreatedAt()
+                    .plusMinutes(5)
+                    .isBefore(LocalDateTime.now())) {
+
+                payment.setStatus(PaymentStatusEnum.EXPIRED);
+                paymentRepo.save(payment);
+            }
+        }
+        
+        PaymentStatusResponse res = new PaymentStatusResponse();
+        res.setStatus(payment.getStatus());
+        // 5. nếu PAID → trả link download
+        if (payment.getStatus() == PaymentStatusEnum.PAID) {
+
+            ProductEntity product = order.getProduct();
+
+            // 🔥 check download hết hạn chưa
+            if (order.getDownloadExpiredAt() != null &&
+                order.getDownloadExpiredAt().isBefore(LocalDateTime.now())) {
+                throw new LogicException("EX","đã hết thời hạn download");
+            }
+
+            // 👉 set expire download nếu chưa có mặc định 3 ngày
+            if (order.getDownloadExpiredAt() == null) {
+                order.setDownloadExpiredAt(LocalDateTime.now().plusDays(3));
+                orderRepo.save(order);
+            }
+
+            // 👉 generate presigned
+			String url = storageService.generatePresignedUrl(product.getFileKey());
+
+            res.setDownloadUrl(url);
+        }
+
+        return res;
 	}
 
 }
