dca1488f0522413eade2b9aa4773340d0c8c3cae
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-02 16:42:19 +0700

    Update PaymentRepository.java

--- marketplace/src/main/java/com/vjshow/marketplace/repository/PaymentRepository.java
+++ marketplace/src/main/java/com/vjshow/marketplace/repository/PaymentRepository.java
@@ -3,9 +3,13 @@ package com.vjshow.marketplace.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.vjshow.marketplace.entity.PaymentEntity;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, String> {	
-	Optional<PaymentEntity> findByContent(String content);
+	
+	@Query("SELECT p FROM PaymentEntity p WHERE :content LIKE CONCAT(p.content, '%')")
+	Optional<PaymentEntity> findByContentInText(@Param("content") String content);
 }
