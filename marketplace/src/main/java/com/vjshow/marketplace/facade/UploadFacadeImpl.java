c976605cb9e28042c383ac78041e52387eb0c588
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-03 15:48:52 +0700

    Update UploadFacadeImpl.java

--- marketplace/src/main/java/com/vjshow/marketplace/facade/UploadFacadeImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/UploadFacadeImpl.java
@@ -3,6 +3,9 @@ package com.vjshow.marketplace.facade;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.vjshow.marketplace.dto.request.CompleteUploadRequest;
 import com.vjshow.marketplace.dto.response.UploadResponse;
@@ -11,6 +14,7 @@ import com.vjshow.marketplace.entity.ProductEntity;
 import com.vjshow.marketplace.service.CloudFlareService;
 import com.vjshow.marketplace.service.CreatorService;
 import com.vjshow.marketplace.service.ProductService;
+import com.vjshow.marketplace.service.WorkerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +28,8 @@ public class UploadFacadeImpl implements UploadFacade{
 	
 	private final ProductService productService;
 	
+	private final WorkerService workerService;
+
 	 @Override
 	    public UploadResponse initUpload(String fileName, Long fileSize, Long userId) {
 
@@ -39,6 +45,7 @@ public class UploadFacadeImpl implements UploadFacade{
 	    }
 
 	    @Override
+	    @Transactional
 	    public ProductEntity completeUpload(CompleteUploadRequest request, Long userId) {
 
 	    	CreatorEntity creator = creatorService.getByUserId(userId);
@@ -48,6 +55,18 @@ public class UploadFacadeImpl implements UploadFacade{
 	        ProductEntity product = productService.createProduct(request, creator);
 
 	        creatorService.increaseStorage(creator, request.getFileSize());
+	        
+	        TransactionSynchronizationManager.registerSynchronization(
+	                new TransactionSynchronization() {
+	                    @Override
+	                    public void afterCommit() {
+	                        workerService.sendJob(
+	                            product.getFileKey(),
+	                            product.getType().name()
+	                        );
+	                    }
+	                }
+	            );
 
 	        return product;
 	    }
