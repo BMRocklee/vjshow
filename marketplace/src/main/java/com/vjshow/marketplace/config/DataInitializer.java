2ee288d1b94f30eccfeccb95eb0244db6a36e40f
duytrinh227 <duytrinh227@gmail.com>
2026-04-05 14:43:15 +0700

    fix

--- marketplace/src/main/java/com/vjshow/marketplace/config/DataInitializer.java
+++ marketplace/src/main/java/com/vjshow/marketplace/config/DataInitializer.java
@@ -17,46 +17,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DataInitializer {
 
-    @Bean
-    public CommandLineRunner initData(UserRepository userRepository,
-                                      CreatorRepository creatorRepository,
-                                      PasswordEncoder passwordEncoder) {
-        return args -> {
-
-            // ===== ADMIN =====
-            if (userRepository.findByEmail("admin@vjshow.vn").isEmpty()) {
-                UserEntity admin = new UserEntity();
-                admin.setEmail("admin@vjshow.vn");
-                admin.setName("Admin");
-                admin.setPassword(passwordEncoder.encode("123456"));
-                admin.setPicture("https://cdn-icons-png.flaticon.com/512/149/149071.png");
-                admin.setRole(Role.ADMIN);
-                admin.setStatus(UserStatusEnum.ACTIVE);
-
-                userRepository.save(admin);
-            }
-
-            // ===== CREATOR =====
-//            if (userRepository.findByEmail("creator@vjshow.vn").isEmpty()) {
+//    @Bean
+//    public CommandLineRunner initData(UserRepository userRepository,
+//                                      CreatorRepository creatorRepository,
+//                                      PasswordEncoder passwordEncoder) {
+//        return args -> {
 //
-//                // tạo user
-//            	UserEntity creatorUser = new UserEntity();
-//                creatorUser.setEmail("creator@vjshow.vn");
-//                creatorUser.setName("Dummy Creator");
-//                creatorUser.setPassword(passwordEncoder.encode("123456"));
-//                creatorUser.setRole(Role.CREATOR);
-//                creatorUser.setStatus(UserStatusEnum.ACTIVE);
+//            // ===== ADMIN =====
+//            if (userRepository.findByEmail("admin@vjshow.vn").isEmpty()) {
+//                UserEntity admin = new UserEntity();
+//                admin.setEmail("admin@vjshow.vn");
+//                admin.setName("Admin");
+//                admin.setPassword(passwordEncoder.encode("123456"));
+//                admin.setPicture("https://cdn-icons-png.flaticon.com/512/149/149071.png");
+//                admin.setRole(Role.ADMIN);
+//                admin.setStatus(UserStatusEnum.ACTIVE);
 //
-//                userRepository.save(creatorUser);
-//
-//                // tạo creator profile
-//                CreatorEntity creator = new CreatorEntity();
-//                creator.setUser(creatorUser);
-//                creator.setEmail("creator@vjshow.vn");
-//                creator.setStatus(CreatorStatus.APPROVED);
-//                creatorRepository.save(creator);
+//                userRepository.save(admin);
 //            }
-        };
-    }
+//
+//        };
+//    }
 
 }
