844f0f516c0227e7fd238922de372b4a3e2d6c0b
duytrinh227 <duytrinh227@gmail.com>
2026-04-05 15:38:32 +0700

    up

--- marketplace/src/main/java/com/vjshow/marketplace/config/DataInitializer.java
+++ marketplace/src/main/java/com/vjshow/marketplace/config/DataInitializer.java
@@ -17,26 +17,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DataInitializer {
 
-//    @Bean
-//    public CommandLineRunner initData(UserRepository userRepository,
-//                                      CreatorRepository creatorRepository,
-//                                      PasswordEncoder passwordEncoder) {
-//        return args -> {
-//
-//            // ===== ADMIN =====
-//            if (userRepository.findByEmail("admin@vjshow.vn").isEmpty()) {
-//                UserEntity admin = new UserEntity();
-//                admin.setEmail("admin@vjshow.vn");
-//                admin.setName("Admin");
-//                admin.setPassword(passwordEncoder.encode("123456"));
-//                admin.setPicture("https://cdn-icons-png.flaticon.com/512/149/149071.png");
-//                admin.setRole(Role.ADMIN);
-//                admin.setStatus(UserStatusEnum.ACTIVE);
-//
-//                userRepository.save(admin);
-//            }
-//
-//        };
-//    }
+    @Bean
+    public CommandLineRunner initData(UserRepository userRepository,
+                                      CreatorRepository creatorRepository,
+                                      PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            // ===== ADMIN =====
+            if (userRepository.findByEmail("admin@vjshow.vn").isEmpty()) {
+                UserEntity admin = new UserEntity();
+                admin.setEmail("admin@vjshow.vn");
+                admin.setName("Admin");
+                admin.setPassword(passwordEncoder.encode("123456"));
+                admin.setPicture("https://cdn-icons-png.flaticon.com/512/149/149071.png");
+                admin.setRole(Role.ADMIN);
+                admin.setStatus(UserStatusEnum.ACTIVE);
+
+                userRepository.save(admin);
+            }
+
+        };
+    }
 
 }
