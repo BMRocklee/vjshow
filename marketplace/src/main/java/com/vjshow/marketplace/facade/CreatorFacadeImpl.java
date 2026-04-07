3119ef4a1dc351aeb1b8ebafb047e58f0922fa84
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-07 11:46:03 +0700

    Update CreatorFacadeImpl.java

--- marketplace/src/main/java/com/vjshow/marketplace/facade/CreatorFacadeImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/CreatorFacadeImpl.java
@@ -1,12 +1,18 @@
 package com.vjshow.marketplace.facade;
 
+import java.util.UUID;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import com.vjshow.marketplace.dto.request.CreatorApplyRequestDTO.CreatorApplyRequest;
+import com.vjshow.marketplace.dto.response.CreatorResponse;
+import com.vjshow.marketplace.dto.response.ProductPageResponse;
 import com.vjshow.marketplace.entity.UserEntity;
+import com.vjshow.marketplace.enums.ProductTypeEnum;
 import com.vjshow.marketplace.exception.LogicException;
 import com.vjshow.marketplace.service.CreatorService;
+import com.vjshow.marketplace.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +21,23 @@ import lombok.RequiredArgsConstructor;
 public class CreatorFacadeImpl implements CreatorFacade {
 	
 	private final CreatorService creatorService;
+	
+	private final ProductService productService;
+
+	@Override
+	public CreatorResponse getCreator(UUID publicId) {
+		return creatorService.getByPublicId(publicId);
+	}
+
+	@Override
+	public ProductPageResponse getProducts(UUID publicId, ProductTypeEnum type, int page, int size) {
+		// validate type nếu cần
+        if (type == null) {
+            type = ProductTypeEnum.IMAGE; // default giống FE
+        }
+
+        return productService.getByCreator(publicId, type, page, size);
+	}
 
 	@Override
 	public void apply(Authentication authentication, CreatorApplyRequest req) {
@@ -24,6 +47,7 @@ public class CreatorFacadeImpl implements CreatorFacade {
 
 		UserEntity userme = (UserEntity) authentication.getPrincipal();
 		creatorService.apply(userme, req);
+		
 	}
 
 }
