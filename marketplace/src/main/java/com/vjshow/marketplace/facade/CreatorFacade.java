ae6c182e4dba1950e9799b191cdfaa312f27a7ec
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-07 11:45:49 +0700

    Update CreatorFacade.java

--- marketplace/src/main/java/com/vjshow/marketplace/facade/CreatorFacade.java
+++ marketplace/src/main/java/com/vjshow/marketplace/facade/CreatorFacade.java
@@ -1,9 +1,19 @@
 package com.vjshow.marketplace.facade;
 
+import java.util.UUID;
+
 import org.springframework.security.core.Authentication;
 
 import com.vjshow.marketplace.dto.request.CreatorApplyRequestDTO.CreatorApplyRequest;
+import com.vjshow.marketplace.dto.response.CreatorResponse;
+import com.vjshow.marketplace.dto.response.ProductPageResponse;
+import com.vjshow.marketplace.enums.ProductTypeEnum;
 
 public interface CreatorFacade {
+
+	public CreatorResponse getCreator(UUID publicId);
+
+	public ProductPageResponse getProducts(UUID publicId, ProductTypeEnum type, int page, int size);
+
 	public void apply(Authentication authentication, CreatorApplyRequest req);
 }
