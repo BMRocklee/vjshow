5d0cda9e8122d9b42b8fc1b3dccaf578f729f064
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-07 11:46:18 +0700

    Update OrderRepository.java

--- marketplace/src/main/java/com/vjshow/marketplace/repository/OrderRepository.java
+++ marketplace/src/main/java/com/vjshow/marketplace/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.vjshow.marketplace.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vjshow.marketplace.entity.OrderEntity;
+import com.vjshow.marketplace.enums.OrderStatusEnum;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 	Optional<OrderEntity> findTopByBuyerIdAndProductIdOrderByCreatedAtDesc(Long buyerId, Long productId);
@@ -12,4 +14,6 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 	Optional<OrderEntity> findByPaymentId(String id);
 	
 	 boolean existsByProduct_Id(Long productId);
+
+	List<OrderEntity> findByBuyerIdAndStatus(Long userId, OrderStatusEnum paid);
 }
