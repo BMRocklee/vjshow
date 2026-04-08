6a7145b47635ec10438d85b3033fa45765664bd7
duytrinh227 <duytrinh227@gmail.com>
2026-04-09 01:09:31 +0700

    up

--- marketplace/src/main/java/com/vjshow/marketplace/service/VietQrGatewayImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/VietQrGatewayImpl.java
@@ -12,6 +12,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.vjshow.marketplace.dto.response.PayOSResponse;
 import com.vjshow.marketplace.entity.PaymentEntity;
 
+import vn.payos.PayOS;
+import vn.payos.model.v2.paymentRequests.CreatePaymentLinkRequest;
+import vn.payos.model.v2.paymentRequests.CreatePaymentLinkResponse;
+
 @Service
 public class VietQrGatewayImpl implements PaymentGatewayService {
 
@@ -32,22 +36,38 @@ public class VietQrGatewayImpl implements PaymentGatewayService {
 
 	@Override
 	public String generatePayOsQR(PaymentEntity payment) {
+		
+	    final PayOS payOS = new PayOS(clientId, apiKey, checkSumKey);
+	    
+	    Long orderCode = System.currentTimeMillis() / 1000;
+        CreatePaymentLinkRequest paymentData =
+            CreatePaymentLinkRequest.builder()
+                .orderCode(orderCode)
+                .amount(payment.getAmount())
+                .description(payment.getContent())
+                .returnUrl("https://vjshow.vn/payment-result")
+                .cancelUrl("https://vjshow.vn/payment-result")
+                .build();
+        
+        CreatePaymentLinkResponse result = payOS.paymentRequests().create(paymentData);
+        return result.getQrCode();
+	    
 
-		WebClient webClient = WebClient.builder().baseUrl("https://api-merchant.payos.vn")
-				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
-
-		Map<String, Object> body = new HashMap<>();
-		body.put("orderCode", payment.getId());
-		body.put("amount", payment.getAmount());
-		body.put("description", payment.getContent());
-		body.put("returnUrl", "https://vjshow.vn/success-create-qr");
-		body.put("cancelUrl", "https://vjshow.vn/error-create-qr");
-
-		PayOSResponse response = webClient.post().uri("/v2/payment-requests").header("x-client-id", clientId)
-				.header("x-api-key", apiKey).bodyValue(body).retrieve().bodyToMono(PayOSResponse.class).block();
-
-		// parse response
-		String url = response.getData().getCheckoutUrl();
+//		WebClient webClient = WebClient.builder().baseUrl("https://api-merchant.payos.vn")
+//				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+//
+//		Map<String, Object> body = new HashMap<>();
+//		body.put("orderCode", payment.getId());
+//		body.put("amount", payment.getAmount());
+//		body.put("description", payment.getContent());
+//		body.put("returnUrl", "https://vjshow.vn/success-create-qr");
+//		body.put("cancelUrl", "https://vjshow.vn/error-create-qr");
+//
+//		PayOSResponse response = webClient.post().uri("/v2/payment-requests").header("x-client-id", clientId)
+//				.header("x-api-key", apiKey).bodyValue(body).retrieve().bodyToMono(PayOSResponse.class).block();
+//
+//		// parse response
+//		String url = response.getData().getCheckoutUrl();
 
 		return url;
 	}
