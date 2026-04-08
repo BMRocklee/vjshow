5294f543ad64bbd3c1c612730f0412d68d649528
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-08 16:10:03 +0700

    Update WebhookService.java

--- marketplace/src/main/java/com/vjshow/marketplace/service/WebhookService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/WebhookService.java
@@ -1,7 +1,10 @@
 package com.vjshow.marketplace.service;
 
 import com.vjshow.marketplace.dto.request.CassoWebhookDto;
+import com.vjshow.marketplace.dto.response.PayOSWebhookDto;
 
 public interface WebhookService {
 	void handleCasso(CassoWebhookDto body);
+	
+	void handlePayOS(PayOSWebhookDto body);
 }
