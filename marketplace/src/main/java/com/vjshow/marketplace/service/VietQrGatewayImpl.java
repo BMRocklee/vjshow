fd955a4b6e3c7764f3f8e2698f4d2e54f8a4aea3
duytrinh227 <duytrinh227@gmail.com>
2026-04-09 01:32:29 +0700

    up

--- marketplace/src/main/java/com/vjshow/marketplace/service/VietQrGatewayImpl.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/VietQrGatewayImpl.java
@@ -1,15 +1,8 @@
 package com.vjshow.marketplace.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import com.vjshow.marketplace.dto.response.PayOSResponse;
 import com.vjshow.marketplace.entity.PaymentEntity;
 
 import vn.payos.PayOS;
@@ -50,25 +43,6 @@ public class VietQrGatewayImpl implements PaymentGatewayService {
                 .build();
         
         CreatePaymentLinkResponse result = payOS.paymentRequests().create(paymentData);
-        return result.getQrCode();
-	    
-
-//		WebClient webClient = WebClient.builder().baseUrl("https://api-merchant.payos.vn")
-//				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
-//
-//		Map<String, Object> body = new HashMap<>();
-//		body.put("orderCode", payment.getId());
-//		body.put("amount", payment.getAmount());
-//		body.put("description", payment.getContent());
-//		body.put("returnUrl", "https://vjshow.vn/success-create-qr");
-//		body.put("cancelUrl", "https://vjshow.vn/error-create-qr");
-//
-//		PayOSResponse response = webClient.post().uri("/v2/payment-requests").header("x-client-id", clientId)
-//				.header("x-api-key", apiKey).bodyValue(body).retrieve().bodyToMono(PayOSResponse.class).block();
-//
-//		// parse response
-//		String url = response.getData().getCheckoutUrl();
-
-		return url;
+        return result.getCheckoutUrl();
 	}
 }
