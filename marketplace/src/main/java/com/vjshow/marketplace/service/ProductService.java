e58e24d62ad243aa483d07ec3e542dd94db1fcd4
Đỗ Duy Trịnh <54642207+BMRocklee@users.noreply.github.com>
2026-04-11 16:54:44 +0700

    Update ProductService.java

--- marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
+++ marketplace/src/main/java/com/vjshow/marketplace/service/ProductService.java
@@ -15,22 +15,21 @@ import com.vjshow.marketplace.enums.ProductTypeEnum;
 public interface ProductService {
 
 	public ProductEntity getById(Long id);
-	
+
 	public ProductEntity createProduct(CompleteUploadRequest request, CreatorEntity creator);
-	
+
 	public List<ProductEntity> getByCreatorId(Long creatorId);
-    
+
 	public ProductEntity update(Long id, ProductRequest request);
-    
+
 	public void delete(Long id);
-	
+
 	public Page<ProductEntity> getPublicProducts(String type, String keyword, int page, int size);
 
 	public List<ProductEntity> getTopProducts(ProductTypeEnum type);
-	
-	public List<ProductEntity> getTopProducts(ProductTypeEnum type, Long quantity);
-	
-	public ProductPageResponse getByCreator(UUID publicId, ProductTypeEnum type, int page, int size);
 
+	public Page<ProductEntity> getTopProducts(ProductTypeEnum type, int page, int size);
+
+	public ProductPageResponse getByCreator(UUID publicId, ProductTypeEnum type, int page, int size);
 
 }
